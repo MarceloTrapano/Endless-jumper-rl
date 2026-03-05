f5a024fd83dde3e716f069d3cc7305b903925789
MarceloTrapano <kacper7866@gmail.com>
2026-03-05 12:09:54 +0100

    first commit

---
+++ Game.py
@@ -0,0 +1,135 @@
+import pygame
+import numpy as np
+
+class Harold(pygame.Rect):
+    def __init__(self, x, y, width, height, image=None):
+        pygame.Rect.__init__(self, x, y, width, height)
+        self.image = image
+        self.airborne = False
+        self.velocity_y = 0
+        self.velocity_x = 0
+    def move(self, gravity, friction, max_speed, cap_y=550, cap_x=500):
+        self.y += self.velocity_y
+        self.velocity_y += gravity
+        
+        if np.abs(self.velocity_x) < friction:
+            self.velocity_x = 0
+        else:
+            self.velocity_x -= friction * np.sign(self.velocity_x)
+        if self.velocity_x > max_speed:
+            self.velocity_x = max_speed
+        if self.velocity_x < -max_speed:
+            self.velocity_x = -max_speed
+        if self.y > cap_y:
+            self.y = cap_y
+            self.airborne = False
+        if self.x > cap_x-20:
+            self.x = cap_x-20
+            self.velocity_x *= -0.6
+        if self.x < 100:
+            self.x = 100
+            self.velocity_x *= -0.6
+        self.x += self.velocity_x
+
+class Block(pygame.Rect):
+    def __init__(self, x, y, width, height=20, image=None):
+        pygame.Rect.__init__(self, x, y, width, height)
+        self.image = image
+        
+
+
+def main():
+    # Global variables
+    INIT_X = 300
+    INIT_Y = 550
+    SHAPE_X = 20
+    SHAPE_Y = 20
+    GRAVITY = 0.5
+    FRICTION = 2
+    COLOR = (255,0,0)
+    COLOR_GRAY = (200,200,200)
+    VELOCITY = 4
+    SCREEN_WIDTH = 600
+    SCREEN_HEIGHT = 600
+    MAX_SPEED = 10
+    MULTIPLIER = 0.8
+    CAMERA_LINE = 300
+    #Boolean variables
+    running = True
+    camera_roll = False
+    #Variables
+    render_distance = 0
+    blocks = {}
+
+    pygame.init()
+
+    # Set up the game window
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Hoty Tower")
+    harold = Harold(INIT_X, INIT_Y, SHAPE_X, SHAPE_Y)
+    # Game loop
+
+    clock = pygame.time.Clock()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]: # Lewo
+            harold.velocity_x -= VELOCITY
+        if keys[pygame.K_d]: # Prawo
+            harold.velocity_x += VELOCITY
+        if keys[pygame.K_w] and not harold.airborne: # Góra
+            harold.velocity_y = -10 - np.abs(harold.velocity_x)*MULTIPLIER
+            harold.airborne = True
+        print(harold.y)
+        
+           
+        if render_distance not in blocks:
+            if render_distance < 80_000:
+                block_w = np.random.randint(5,30)*10
+                block_x = -100
+                while block_x + block_w > 500 or block_x == -100:
+                    block_x = np.random.randint(10, 45)*10
+                blocks[render_distance] = Block(block_x, 480 - render_distance, block_w)
+                render_distance += 80
+
+
+        screen.fill((0, 0, 0))
+        if camera_roll:
+            if harold.y < 200:
+                scroll = 200 - harold.y
+                harold.y = 200
+                for block in blocks.values():
+                    block.y += scroll
+        for block in blocks.values():
+            pygame.draw.rect(screen, COLOR_GRAY, block)
+            if harold.velocity_y > 0 and harold.colliderect(block):
+                if harold.bottom - harold.velocity_y <= block.top:
+                    harold.bottom = block.top
+                    harold.velocity_y = 0
+                    harold.airborne = False
+            if camera_roll:
+                block.y += 1
+                    
+
+        if not camera_roll:
+            if harold.y < CAMERA_LINE:
+                camera_roll = True
+            harold.move(GRAVITY, FRICTION, MAX_SPEED)
+        else:
+            harold.move(GRAVITY, FRICTION, MAX_SPEED, cap_y=700) 
+        
+        if harold.y > 600:
+            running = False
+        
+        pygame.draw.rect(screen, COLOR, harold)
+        pygame.display.flip()
+        clock.tick(60)
+
+
+    # Quit Pygame
+    pygame.quit()
+
+if __name__=="__main__":
+    main()
